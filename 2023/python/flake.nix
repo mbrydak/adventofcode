4c60f18f687379d67021b5c2d16e320c55ce628b
mbrydak <maxbrydak@gmail.com>
2023-12-04 22:26:14 +0100

    update python flake

--- 2023/python/flake.nix
+++ 2023/python/flake.nix
@@ -14,7 +14,7 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [ python311 ] ++
-            (with pkgs.python311Packages; [ pip virtualenv]);
+            (with pkgs.python311Packages; [ pytest pytest-watch ]);
         };
       });
     };
